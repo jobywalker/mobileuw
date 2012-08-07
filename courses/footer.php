4ebcfd785bdfd54de7b61fff0b3bede1a84c733f
jodytate <jtate@uw.edu>
2012-08-07 11:28:09 -0700

    committing deletes

--- courses/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-<!-- CDN Javascript -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="//www.washington.edu/common/bootstrap-2.0.4/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="custom.js"></script>
-<script type="text/javascript" src="common.js"></script>
-</body>
-</html>
