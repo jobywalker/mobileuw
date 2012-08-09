a4fc3c0dad926fafc6f06b81dd7daa63e4acfb33
jodytate <jtate@uw.edu>
2012-08-09 11:23:49 -0700

    some jslinting, some alt attributes
    likely index.html as is won't be used, so could be a futile exercise;
    jslinting more for maintainability than anything else

--- js/common.js
+++ js/common.js
@@ -1,75 +1,76 @@
 $(window).load(function () {
 	var alerty = $('#alertMessage').outerHeight();
-	$("body").css("background-position", "0 " + alerty + "px" ); 
+	$("body").css("background-position", "0" + alerty + "px");
 });
 
 $(function () {
 
-  $('[href=#]').removeAttr('href'); //prevent ios nav bar from popping down
-  
-  var lip = $('#lip'),
-      linkRotator = $('#linkRotator'),
-      ul = linkRotator.find('ul').first(),
-      linkImage = $('#linkArrowIcon'),
-      topnav = $('#top-nav'),
-      search = $('#search form'),
-      links = linkImage.attr('src'),
-      closeImage = $('#menuClose'),
-      searchbtn = $("#searchicon").wrap('<a id="searchicon-wrapper">').parent(),
-      menubtn= $("#listicon").wrap('<a id="listicon-wrapper">').parent();
+    $('[href=#]').removeAttr('href'); //prevent ios nav bar from popping down
+
+    var lip = $('#lip'),
+        linkRotator = $('#linkRotator'),
+        ul = linkRotator.find('ul').first(),
+        linkImage = $('#linkArrowIcon'),
+        topnav = $('#top-nav'),
+        search = $('#search form'),
+        links = linkImage.attr('src'),
+        closeImage = $('#menuClose'),
+        searchbtn = $("#searchicon").wrap('<a id="searchicon-wrapper">').parent(),
+        menubtn = $("#listicon").wrap('<a id="listicon-wrapper">').parent();
 
 	ul.hide();
 	linkImage.click(function () {
 	    ul.show();
-		var height = (linkRotator.height() != 0) ? 0 : ul.outerHeight();
+		var height = (linkRotator.height() !== 0) ? 0 : ul.outerHeight();
 		linkImage.addClass('hideLinkAnchor').one('webkitTransitionEnd', function () {
-			  linkRotator.toggleClass('rotateLip').css('height', height);
-		});
+    		linkRotator.toggleClass('rotateLip').css('height', height);
+    		});
 		return false;
 	});
-	
+
 	closeImage.click(function () {
-		if (linkRotator.height() != 0){
+		if (linkRotator.height() !== 0) {
 			linkRotator.css('height', 0).one('webkitTransitionEnd', function () {				
 				ul.hide();
 				linkImage.removeClass('hideLinkAnchor');
 			});
-		} 
+		}
 		return false;
 	});
 
-  // [TODO] clean up
-	searchbtn.click(function (e){
-    e.preventDefault();
+	// [TODO] clean up
+	searchbtn.click(function (e) {
+        e.preventDefault();
 		search.toggleClass("activate");
-    search.css('visibility', 'visible');
-		if(topnav.hasClass('activate')){
+		search.css('visibility', 'visible');
+		if (topnav.hasClass('activate')) {
 			topnav.toggleClass("activate");		
-		}			
+		}		
 		return false;
 	});
 	
-	menubtn.click(function (e){
-    e.preventDefault();
+	menubtn.click(function (e) {
+        e.preventDefault();
 		topnav.toggleClass("activate");		 
-    topnav.css('visibility', 'visible');
-		if(search.hasClass('activate')){
+		topnav.css('visibility', 'visible');
+		if (search.hasClass('activate')) {
 			search.toggleClass("activate");		
 		}		
 		return false;
 	});
 	
-  /** Accessibility **/
-  topnav.css('visibility', 'hidden').on('webkitTransitionEnd', function () {
-    if ( !topnav.hasClass('activate') && !search.hasClass('activate') || search.hasClass('activate')) {
+	/** Accessibility **/
+	
+	topnav.css('visibility', 'hidden').on('webkitTransitionEnd', function () {if (!topnav.hasClass('activate') && !search.hasClass('activate') || search.hasClass('activate')) {
        topnav.css('visibility', 'hidden');
     }   
-  });
-  search.css('visibility', 'hidden').on('webkitTransitionEnd', function () {
-    if ( !topnav.hasClass('activate') && !search.hasClass('activate') || topnav.hasClass('activate')) {
-       search.css('visibility', 'hidden');
-    }
-  });
+    });
+    
+    search.css('visibility', 'hidden').on('webkitTransitionEnd', function () {
+        if (!topnav.hasClass('activate') && !search.hasClass('activate') || topnav.hasClass('activate')) {
+            search.css('visibility', 'hidden');
+        }
+    });
   
 });
 
